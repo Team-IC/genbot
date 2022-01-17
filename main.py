9146dba3438c61c283f15c4d4f2dae29bfd69658
Kartik OP <85619363+IndianDevInc@users.noreply.github.com>
2022-01-17 10:18:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix=prefix, intents=intents)
 
-bypass_id = [887274088249323560]
+bypass_id = [Your_ID_here]
 
 @bot.event
 async def on_ready():
